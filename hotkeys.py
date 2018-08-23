eddc2ac1241d98296cdfd9d820b3c663d783ac2f
Seth-Revz <sethmorris.dev@gmail.com>
2018-08-23 17:07:11 -0400

    Added error msg if bl2 isnt running (wont run hotkey)

--- hotkeys.py
+++ hotkeys.py
@@ -1,66 +1,104 @@
 import keyboard
 import time
+#import subprocess
+import win32gui
+from main import MainWindow
 
 def execPatch():
-    keyboard.send('`')
-    keyboard.send('e')
-    keyboard.send('x')
-    keyboard.send('e')
-    keyboard.send('c')
-    keyboard.send('space')
-    keyboard.send('p')
-    keyboard.send('a')
-    keyboard.send('t')
-    keyboard.send('c')
-    keyboard.send('h')
-    keyboard.send('.')
-    keyboard.send('t')
-    keyboard.send('x')
-    keyboard.send('t')
-    keyboard.send('enter')
-    keyboard.send('`')
+    handle = win32gui.FindWindow(0, "Borderlands 2 (32-bit, DX9)")
+    if (handle):
+        win32gui.SetForegroundWindow(handle)
+        keyboard.send('`')
+        keyboard.send('e')
+        keyboard.send('x')
+        keyboard.send('e')
+        keyboard.send('c')
+        keyboard.send('space')
+        keyboard.send('p')
+        keyboard.send('a')
+        keyboard.send('t')
+        keyboard.send('c')
+        keyboard.send('h')
+        keyboard.send('.')
+        keyboard.send('t')
+        keyboard.send('x')
+        keyboard.send('t')
+        keyboard.send('enter')
+        keyboard.send('`')
+    else:
+        MainWindow.errorMsg("open")
 
 def colorblindFix():
-    keyboard.send('escape')
-    time.sleep(.02)
-    keyboard.send('down'); keyboard.send('down')
-    keyboard.send('enter')
-    time.sleep(.035)
-    keyboard.send('down'); keyboard.send('down')
-    keyboard.send('enter')
-    time.sleep(.02)
-    keyboard.send('down'); keyboard.send('down'); keyboard.send('down'); keyboard.send('down')
-    keyboard.send('down'); keyboard.send('down'); keyboard.send('down'); keyboard.send('down')
-    keyboard.send('down'); keyboard.send('down'); keyboard.send('down'); keyboard.send('down')
-    keyboard.send('down'); keyboard.send('down'); keyboard.send('down'); keyboard.send('down')
-    keyboard.send('down'); keyboard.send('down'); keyboard.send('down'); keyboard.send('down')
-    keyboard.send('down')
-    keyboard.send('right'); keyboard.send('right')
-    time.sleep(.05)
-    keyboard.send('escape'); keyboard.send('escape'); keyboard.send('escape')
+    handle = win32gui.FindWindow(0, "Borderlands 2 (32-bit, DX9)")
+    if (handle):
+        keyboard.send('escape')
+        time.sleep(.02)
+        keyboard.send('down'); keyboard.send('down')
+        keyboard.send('enter')
+        time.sleep(.035)
+        keyboard.send('down'); keyboard.send('down')
+        keyboard.send('enter')
+        time.sleep(.02)
+        keyboard.send('down'); keyboard.send('down'); keyboard.send('down'); keyboard.send('down')
+        keyboard.send('down'); keyboard.send('down'); keyboard.send('down'); keyboard.send('down')
+        keyboard.send('down'); keyboard.send('down'); keyboard.send('down'); keyboard.send('down')
+        keyboard.send('down'); keyboard.send('down'); keyboard.send('down'); keyboard.send('down')
+        keyboard.send('down'); keyboard.send('down'); keyboard.send('down'); keyboard.send('down')
+        keyboard.send('down')
+        keyboard.send('right'); keyboard.send('right')
+        time.sleep(.05)
+        keyboard.send('escape'); keyboard.send('escape'); keyboard.send('escape')
+    else:
+        MainWindow.errorMsg("open")
 
 def reload():
-    keyboard.send('escape')
-    time.sleep(.02)
-    keyboard.send('down'); keyboard.send('down'); keyboard.send('down'); keyboard.send('down')
-    keyboard.send('enter')
-    time.sleep(.05)
-    keyboard.send('up')
-    keyboard.send('enter')
-    time.sleep(2)
-    keyboard.send('enter')
-    time.sleep(.05)
-    keyboard.send('enter')
+    handle = win32gui.FindWindow(0, "Borderlands 2 (32-bit, DX9)")
+    if (handle):
+        keyboard.send('escape')
+        time.sleep(.02)
+        keyboard.send('down'); keyboard.send('down'); keyboard.send('down'); keyboard.send('down')
+        keyboard.send('enter')
+        time.sleep(.05)
+        keyboard.send('up')
+        keyboard.send('enter')
+        time.sleep(2)
+        keyboard.send('enter')
+        time.sleep(.05)
+        keyboard.send('enter')
+    else:
+        MainWindow.errorMsg("open")
 
 '''
 def readOnlyReload():
-    keyboard.send('escape')
-    time.sleep(.02)
-    keyboard.send('down'); keyboard.send('down'); keyboard.send('down'); keyboard.send('down')
-    keyboard.send('enter')
-    time.sleep(.05)
-    keyboard.send('up')
-    keyboard.send('enter')
-    time.sleep(1.8)
-    keyboard.send('down'); keyboard.send('down'); keyboard.send('down'); keyboard.send('down')
+    handle = win32gui.FindWindow(0, "Borderlands 2 (32-bit, DX9)")
+    if (handle):
+        keyboard.send('escape')
+        time.sleep(.02)
+        keyboard.send('down'); keyboard.send('down'); keyboard.send('down'); keyboard.send('down')
+        keyboard.send('enter')
+        time.sleep(.05)
+        keyboard.send('up')
+        keyboard.send('enter')
+        time.sleep(1.8)
+        keyboard.send('down'); keyboard.send('down'); keyboard.send('down'); keyboard.send('down')
+    else:
+        MainWindow.errorMsg("open")
+'''
+
+''' Used for testing
+def window_enum_handler(hwnd, resultList):
+    if win32gui.IsWindowVisible(hwnd) and win32gui.GetWindowText(hwnd) != '':
+        resultList.append((hwnd, win32gui.GetWindowText(hwnd)))
+
+def get_app_list(handles=[]):
+    mlst=[]
+    win32gui.EnumWindows(window_enum_handler, handles)
+    for handle in handles:
+        mlst.append(handle)
+    return mlst
+
+def setActiveWindow():
+    appwindows = get_app_list()
+    for i in appwindows:
+        print(i)
 '''
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import keyboard
 import hotkeys
-from Gui import Ui_MainWindow
+from gui import Ui_MainWindow
 from PyQt5 import QtGui, QtWidgets
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -11,6 +11,18 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+    def aboutAction(self):
+        QtWidgets.QMessageBox.about(self, "About", "\n--Borderlands Helper Alpha--\n\nhttps://github.com/Seth-Revz")
+
+    def errorMsg(event=""):
+        if event == "open":
+            error = QtWidgets.QMessageBox()
+            error.setWindowTitle("Error")
+            error.setText("Borderlands2 is not running")
+            error.setIcon(QtWidgets.QMessageBox.Warning)
+            error.setStandardButtons(QtWidgets.QMessageBox.Ok)
+            error.exec()
+
 if __name__ == "__main__":
     
     keyboard.add_hotkey("F5", lambda: keyboard.call_later(hotkeys.execPatch), suppress=True)
@@ -18,7 +30,7 @@ if __name__ == "__main__":
     keyboard.add_hotkey("F7", lambda: keyboard.call_later(hotkeys.reload), suppress=True)
     #keyboard.add_hotkey("F8", lambda: keyboard.call_later(hotkeys.readOnlyReload), suppress=True)
     keyboard.add_hotkey('end', lambda: app.exit(), suppress=True)
-    
+
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = MainWindow()
     MainWindow.show()
