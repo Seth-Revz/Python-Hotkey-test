b3a07c84d12f541bc3250ed61434d37c67a7e4aa
Seth-Revz <sethmorris.dev@gmail.com>
2018-08-22 20:39:56 -0400

    Created gui with designer

---
+++ Gui.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'untitled.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+import hotkeys
+
+class Ui_MainWindow(QtWidgets.QMainWindow):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(300, 125)
+        MainWindow.setWindowIcon(QtGui.QIcon("resources\BL2Revz.ico"))
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(10, 10, 101, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(120, 10, 111, 23))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(250, 10, 31, 21))
+        self.label.setStatusTip("")
+        self.label.setWhatsThis("")
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.groupBox = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox.setGeometry(QtCore.QRect(10, 40, 271, 51))
+        self.groupBox.setObjectName("groupBox")
+        self.pushButton_3 = QtWidgets.QPushButton(self.groupBox)
+        self.pushButton_3.setGeometry(QtCore.QRect(10, 20, 61, 23))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.radioButton = QtWidgets.QRadioButton(self.groupBox)
+        self.radioButton.setGeometry(QtCore.QRect(80, 20, 81, 21))
+        self.radioButton.setObjectName("radioButton")
+        self.radioButton.setChecked(True)
+        self.radioButton_2 = QtWidgets.QRadioButton(self.groupBox)
+        self.radioButton_2.setGeometry(QtCore.QRect(170, 20, 101, 21))
+        self.radioButton_2.setObjectName("radioButton_2")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 300, 21))
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
+        self.menuHelp = QtWidgets.QMenu(self.menubar)
+        self.menuHelp.setObjectName("menuHelp")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionAbout = QtWidgets.QAction(MainWindow)
+        self.actionAbout.setObjectName("actionAbout")
+        self.actionExit = QtWidgets.QAction(MainWindow)
+        self.actionExit.setObjectName("actionExit")
+        self.menuFile.addAction(self.actionExit)
+        self.menuHelp.addAction(self.actionAbout)
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menubar.addAction(self.menuHelp.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Borderlands Helper"))
+        self.pushButton.setText(_translate("MainWindow", "Exec Patch [F5]"))
+        self.pushButton.clicked.connect(hotkeys.execPatch)
+        self.pushButton_2.setText(_translate("MainWindow", "Fix Colorblind [F6]"))
+        self.label.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" color:#380af0;\">Revz</span></p></body></html>"))
+        self.groupBox.setTitle(_translate("MainWindow", "Reload"))
+        self.pushButton_3.setText(_translate("MainWindow", "Reload"))
+        self.radioButton.setText(_translate("MainWindow", "Normal [F7]"))
+        self.radioButton_2.setText(_translate("MainWindow", "Read Only [F8]"))
+        self.menuFile.setTitle(_translate("MainWindow", "File"))
+        self.menuHelp.setTitle(_translate("MainWindow", "Help"))
+        self.actionAbout.setText(_translate("MainWindow", "About"))
+        self.actionExit.setText(_translate("MainWindow", "Exit"))
+
--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import sys
 import keyboard
 import hotkeys
-
+from Gui import Ui_MainWindow
 from PyQt5 import QtGui, QtWidgets
 
-app = QtWidgets.QApplication(sys.argv)
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
 
-MainWindow = QtWidgets.QMainWindow()
-MainWindow.show()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
-keyboard.add_hotkey("F5", lambda: keyboard.call_later(hotkeys.execPatch), suppress=True)
-keyboard.add_hotkey("F6", lambda: keyboard.call_later(hotkeys.colorblindFix), suppress=True)
-keyboard.add_hotkey("F7", lambda: keyboard.call_later(hotkeys.reload), suppress=True)
-#keyboard.add_hotkey("F8", lambda: keyboard.call_later(hotkeys.readOnlyReload), suppress=True)
-keyboard.add_hotkey('end', lambda: app.exit(), suppress=True)
-
-app.exec()
+if __name__ == "__main__":
+    
+    keyboard.add_hotkey("F5", lambda: keyboard.call_later(hotkeys.execPatch), suppress=True)
+    keyboard.add_hotkey("F6", lambda: keyboard.call_later(hotkeys.colorblindFix), suppress=True)
+    keyboard.add_hotkey("F7", lambda: keyboard.call_later(hotkeys.reload), suppress=True)
+    #keyboard.add_hotkey("F8", lambda: keyboard.call_later(hotkeys.readOnlyReload), suppress=True)
+    keyboard.add_hotkey('end', lambda: app.exit(), suppress=True)
+    
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = MainWindow()
+    MainWindow.show()
+    app.exec()
